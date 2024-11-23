915ab1fa2f755fdb7f022af8a4252e5c07529a28
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-11-23 16:33:02 -0300

    feat: Tabela associando usuários e queries

---
+++ migrations/V27__create_table_usuarios_queries.sql
@@ -0,0 +1,12 @@
+USE `dataviz_bytelabss`;
+
+CREATE TABLE `usuarios_custom_query` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `usuario` bigint DEFAULT NULL,
+  `custom_query` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `usuario` (`usuario`),
+  KEY `custom_query` (`custom_query`),
+  CONSTRAINT `usuarios_custom_query_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuarios` (`id`),
+  CONSTRAINT `usuarios_custom_query_ibfk_2` FOREIGN KEY (`custom_query`) REFERENCES `custom_queries` (`id`)
+);
