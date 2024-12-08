63a18409c97660a55cea99302fe7da36be46629c
larissa-fernanda <laliffr@gmail.com>
2024-12-08 12:58:33 -0300

    refactor: alterar estrutura do sql v24

--- migrations/V24__Insert_into_graficos.sql
+++ migrations/V24__Insert_into_graficos.sql
@@ -1,1 +1,5 @@
-INSERT INTO `graficos` VALUES (1,NULL,50,0,_binary '','Tempo médio de contratação por vaga'),(2,'2024-10-24 23:59:59',20,0,_binary '','Quantidade de Contratações por Participante RH'),(3,'2024-10-26 23:59:59',20,0,_binary '','Tempo Médio de Contratação por Processo Seletivo'),(4,NULL,100,0,_binary '\0',NULL);
+INSERT INTO `graficos` VALUES 
+    (1,NULL,50,0,_binary '','Tempo médio de contratação por vaga'),
+    (2,'2024-10-24 23:59:59',20,0,_binary '','Quantidade de Contratações por Participante RH'),
+    (3,'2024-10-26 23:59:59',20,0,_binary '','Tempo Médio de Contratação por Processo Seletivo'),
+    (4,NULL,100,0,_binary '\0',NULL);
