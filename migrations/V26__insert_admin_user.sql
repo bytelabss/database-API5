cd9de6cd8c064b9f77635be43e4fe40e3af6f43c
larissa-fernanda <laliffr@gmail.com>
2024-12-08 12:58:47 -0300

    refactor: alterar estrutura do sql v26

--- migrations/V26__insert_admin_user.sql
+++ migrations/V26__insert_admin_user.sql
@@ -1,7 +1,14 @@
-USE `dataviz_bytelabss`;
+INSERT INTO 
+    `usuarios` (`id`, `name`, `encrypted_password`) 
+VALUES 
+    (1, 'admin', '$2a$12$zXM1Fo1d24cXFiZ8.TVx3uViSd3svxeMGTwCBZAuBY7ZUZCkQqtEm');
 
-INSERT INTO `usuarios` (`id`, `name`, `encrypted_password`) VALUES (1, 'admin', '$2a$12$zXM1Fo1d24cXFiZ8.TVx3uViSd3svxeMGTwCBZAuBY7ZUZCkQqtEm');
+INSERT INTO 
+    `autorizacoes` (`id`, `name`) 
+VALUES 
+    (1, 'ADMINISTRADOR');
 
-INSERT INTO `autorizacoes` (`id`, `name`) VALUES (1, 'ADMINISTRADOR');
-
-INSERT INTO `usuarios_autorizacoes` (`usuario_id`, `autorizacao_id`) VALUES (1, 1);
+INSERT INTO 
+    `usuarios_autorizacoes` (`usuario_id`, `autorizacao_id`) 
+VALUES 
+    (1, 1);
